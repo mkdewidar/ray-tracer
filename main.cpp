b7950aab100f8fdc34a319731b0fe26b5c4c4b6f
mkdewidar <mkdewidar@gmail.com>
2020-10-15 01:46:52 +0300

    Render basic image to stdout and log progress to stderr

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <thread>
+
+
+using namespace std::chrono_literals;
+
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    const int imageWidth = 256;
+    const int imageHeight = 256;
+
+    std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+
+    for (int j = imageHeight - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << std::flush;
+
+        // uncomment the line below to slow the rendering and see the progress bar
+        // std::this_thread::sleep_for(50ms);
+
+        for (int i = 0; i < imageWidth; ++i) {
+            int R = static_cast<int>(i);
+            int G = static_cast<int>(j);
+            int B = static_cast<int>(0);
+
+            std::cout << R << " " << G << " " << B << "\n";
+        }
+    }
+
+    std::cerr << "\nDone\n";
 
     return 0;
 }
